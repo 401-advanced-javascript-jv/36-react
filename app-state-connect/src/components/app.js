31b4f1ec4c175462563fc78293e81bc8c7da18d1
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-01 21:43:37 -0700

    index connected to redux store

--- app-state-connect/src/components/app.js
+++ app-state-connect/src/components/app.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo:'bar'
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     foo: 'bar',
+  //   };
+  // }
 
   handleChange = () => {
-    this.setState({foo:Math.random()});
-  }
+    // this.setState({ foo: Math.random() });
+  };
 
   render() {
-    return (
-      <div onClick={this.handleChange}>
-        {this.state.foo}
-      </div>
-    );
+    return <div onClick={this.handleChange}>{this.props.foo}</div>;
   }
 }
 
-export default App;
+const mapStoreToPropsAsState = (state) => ({
+  foo: 'bar',
+});
+
+export default connect(mapStoreToPropsAsState)(App);
